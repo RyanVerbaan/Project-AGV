c82193b7b1b3d915f9a034e34e572f5ef33667f9
RyanVerbaan <ryan.verbaan@hotmail.com>
2019-05-24 00:26:36 +0200

    .

--- Project/Project.ino
+++ Project/Project.ino
@@ -1,10 +1,26 @@
+#include <Stepper.h>
+
+
+//Defines voor stepper motor(A_Mega)
+#define STEPS     200
+  //Pinnen voor linker stepper
+#define Lpin_ain1  22
+#define Lpin_ain2  23
+#define Lpin_bin1  24
+#define Lpin_bin2  25
+  //Pinnen voor retcher stepper
+#define Rpin_ain1  28
+#define Rpin_ain2  29
+#define Rpin_bin1  30
+#define Rpin_bin2  31
+
+Stepper stepper_links(STEPS, Lpin_ain2, Lpin_ain1, Lpin_bin1, Lpin_bin2);
+Stepper stepper_rechts((STEPS, Rpin_ain2, Rpin_ain1, Rpin_bin1, Rpin_bin2););
+
 void setup() {
-  char morgen = "goed";
-  Hoi, ik ben jep  
 
 }
 
 void loop() {
-  if(morgen == "goed")
-    Serial.println("goede morgen")
+  
 }
--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -1,27 +1,37 @@
-#include <Stepper.h>
- 
-// change this to the number of steps on your motor
-#define STEPS 200
- 
-// create an instance of the stepper class, specifying
-// the number of steps of the motor and the pins it's
-// attached to
-Stepper stepper(STEPS, 4, 5, 6, 7);
+#include <Stepper.h> 
+
+//Defines voor stepper motor(A_Mega)
+  //Aantal steps op de motor
+#define STEPS     200
+  //Pinnen voor linker stepper
+#define Lpin_ain1  22
+#define Lpin_ain2  23
+#define Lpin_bin1  24
+#define Lpin_bin2  25
+  //Pinnen voor retcher stepper
+#define Rpin_ain1  28
+#define Rpin_ain2  29
+#define Rpin_bin1  30
+#define Rpin_bin2  31
+
+Stepper stepper_links(STEPS, Lpin_ain2, Lpin_ain1, Lpin_bin1, Lpin_bin2);
+Stepper stepper_rechts(STEPS, Rpin_ain2, Rpin_ain1, Rpin_bin1, Rpin_bin2);
  
  
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Stepper test!");
+  
   // set the speed of the motor to 30 RPMs
-  stepper.setSpeed(60);
+  stepper_links.setSpeed(60);
   
 }
  
 void loop()
 {
-  //Serial.println("Forward");
-  stepper.step(STEPS);
+  Serial.println("Forward");
+  stepper_links.step(STEPS);
   //Serial.println("Backward");
   //stepper.step(-STEPS);
 }
