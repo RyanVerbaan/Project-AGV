f5c1e376bbb1740ece48ae2ca47be82f3746fa33
RyanVerbaan <ryan.verbaan@hotmail.com>
2019-05-26 23:45:03 +0200

    new

--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -4,22 +4,28 @@
   //Aantal steps op de motor
 #define STEPS     200
   //Pinnen voor linker stepper
-#define Lpin_ain1  22
-#define Lpin_ain2  23
-#define Lpin_bin1  24
-#define Lpin_bin2  25
+#define Lpin_ain1  4
+#define Lpin_ain2  5
+#define Lpin_bin1  6
+#define Lpin_bin2  7
   //Pinnen voor retcher stepper
 #define Rpin_ain1  28
 #define Rpin_ain2  29
 #define Rpin_bin1  30
 #define Rpin_bin2  31
 
+#define knopje     A8
+
+
 Stepper stepper_links(STEPS, Lpin_ain2, Lpin_ain1, Lpin_bin1, Lpin_bin2);
 Stepper stepper_rechts(STEPS, Rpin_ain2, Rpin_ain1, Rpin_bin1, Rpin_bin2);
  
- 
+int val = 0;
+
 void setup()
 {
+  
+  pinMode(knopje, INPUT);
   Serial.begin(9600);
   Serial.println("Stepper test!");
   
@@ -30,8 +36,17 @@ void setup()
  
 void loop()
 {
-  Serial.println("Forward");
-  stepper_links.step(STEPS);
+  stepper_links.step(1);
+  val = analogRead(knopje);
+  Serial.println(analogRead(knopje));
+  if(val >= 900)
+  {
+    stepper_links.setSpeed(60);
+  }
+  else{
+    stepper_links.setSpeed(5);
+  }
+  delay(1);
   //Serial.println("Backward");
   //stepper.step(-STEPS);
 }
