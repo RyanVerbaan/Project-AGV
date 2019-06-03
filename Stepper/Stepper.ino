93dea8b5684db6e338139b2b730235a00e32b91c
RyanVerbaan <ryan.verbaan@hotmail.com>
2019-06-04 00:11:59 +0200

    .

--- Stepper/Stepper.ino
+++ Stepper/Stepper.ino
@@ -4,26 +4,24 @@
 //Aantal steps op de motor
 #define STEPS     200
 //Pinnen voor linker stepper  //Mogen digitale pinnen zijn
-#define Lpin_ain2  22
-#define Lpin_ain1  23
-#define Lpin_bin1  24
-#define Lpin_bin2  25
+#define Pin_Links_Ain2  22
+#define Pin_Links_Ain1  23
+#define Pin_Links_Bin1  24
+#define Pin_Links_Bin2  25
 //Pinnen voor retcher stepper
-#define Rpin_ain2  28
-#define Rpin_ain1  29
-#define Rpin_bin1  30
-#define Rpin_bin2  31
+#define Pin_Rechts_Ain2  28
+#define Pin_Rechts_Ain1  29
+#define Pin_Rechts_Bin1  30
+#define Pin_Rechts_Bin2  31
 
 #define Motor_speed_max     120                 //100rpm is max reacheble speed op 5V
 #define Motor_speed_follow  (Motor_speed_max/7) //70% speed for following person
 #define Motor_speed_half    (Motor_speed_max/2)
 #define Motor_speed_stop    0                   //Set motor stil but is still running
-#define Aantal_rond         2*STEPS
-#define Aantal_rond         3*STEPS
 
 
-Stepper stepper_links(STEPS, Lpin_ain2, Lpin_ain1, Lpin_bin1, Lpin_bin2);
-Stepper stepper_rechts(STEPS, Rpin_ain2, Rpin_ain1, Rpin_bin1, Rpin_bin2);
+Stepper stepper_links(STEPS, Pin_Links_Ain2, Pin_Links_Ain1, Pin_Links_Bin1, Pin_Links_Bin2);
+Stepper stepper_rechts(STEPS, Pin_Rechts_Ain2, Pin_Rechts_Ain1, Pin_Rechts_Bin1, Pin_Rechts_Bin2);
 
 
 //void Bocht(value voor bocht links of rechts);
@@ -36,26 +34,20 @@ void setup()
 {
 
   Serial.begin(9600);
-  Serial.println("Stepper test!");
-
-  // set the speed of the motor to 30 RPMs
-  
+  Serial.println("Stepper test!")
+  stepper_links.setSpeed(120);
+  stepper_rechts.setSpeed(120);
 
 }
 
 void loop()
 {
-  stepper_links.setSpeed(120);
-  stepper_rechts.setSpeed(120);
- //for(i = 0; i<Aantal_rond; i++) //laat hopelijk beide wielen tegelijk rijden
- //{
+
   stepper_links.step(1);
   stepper_rechts.step(1);
-  //delay(0); // max delay tussen steps around 6~7 miliseconde
- //}
+  
 }
 /*
-
 void Bocht(value voor bocht links of rechts)
 {
   //ToF sensoren uit
